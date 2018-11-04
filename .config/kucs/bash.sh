747595a52e19011e271f986e352c088d9862ba09
koutsie <github@qoutsy.33mail.com>
2018-11-04 15:17:39 +0200

    Removed aliasme

--- .config/kucs/bash.sh
+++ .config/kucs/bash.sh
@@ -1,6 +1,5 @@
 #Kucs startup file: add this to "bash /home/$USER/.config/kucs/bash.sh (and make it executeable by doing chmod +x bash.sh)
 cd /home/$USER/scripts && echo "Scripts installed: $(ls -a | wc -l)" | lolcat-c
-source ~/.aliasme/aliasme.sh
 cd $home
 echo "---" | lolcat-c
 IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
